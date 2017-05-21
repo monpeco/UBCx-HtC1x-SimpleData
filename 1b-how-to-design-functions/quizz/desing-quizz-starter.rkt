03c1c09542d85ee86cdac5e972a44c28fd13f588
monpeco <monpeco@gmail.com>
2017-05-21 17:30:32 -0400

    Update on design quizz based on evaluation tutorial

--- 1b-how-to-design-functions/quizz/desing-quizz-starter.rkt
+++ 1b-how-to-design-functions/quizz/desing-quizz-starter.rkt
@@ -2,38 +2,27 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname desing-quizz-starter) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (require 2htdp/image)
-;; design-quizz.rkt
 
-;;PROBLEM:
-;;Design a function that consumes two images and produces true if the first is larger
-;;than the second.
+;; Image Image -> Boolean                                                ;(signature)
+;; Returns true if the first image is larger that the second             ;(purpose)
 
-;;Signature, purpose and stub.
-;;Define examples, wrap each in check-expect.
-;;Template and inventory.
-;;Code the function body.
-;;Test and debug until correct
+(check-expect (image-larger? (rectangle 10 10 "solid" "red") (rectangle 20 20 "solid" "red") ) false)  ;(examples)
+(check-expect (image-larger? (rectangle 20 10 "solid" "red") (rectangle 20 20 "solid" "red") ) false)
+(check-expect (image-larger? (rectangle 30 10 "solid" "red") (rectangle 20 20 "solid" "red") ) false)
 
-(check-expect (image-larger? (rectangle 0 0 "solid" "red")                   ;(examples)
-                            (rectangle 0 0 "solid" "red") ) false)
+(check-expect (image-larger? (rectangle 10 20 "solid" "red") (rectangle 20 20 "solid" "red") ) false)
+(check-expect (image-larger? (rectangle 20 20 "solid" "red") (rectangle 20 20 "solid" "red") ) false)
+(check-expect (image-larger? (rectangle 30 20 "solid" "red") (rectangle 20 20 "solid" "red") ) false)
 
-(check-expect (image-larger? (rectangle 10 10 "solid" "red")
-                            (rectangle 10 10 "solid" "red") ) false)
+(check-expect (image-larger? (rectangle 10 30 "solid" "red") (rectangle 20 20 "solid" "red") ) false)
+(check-expect (image-larger? (rectangle 20 30 "solid" "red") (rectangle 20 20 "solid" "red") ) false)
+(check-expect (image-larger? (rectangle 30 30 "solid" "red") (rectangle 20 20 "solid" "red") ) true)
 
-(check-expect (image-larger? (rectangle 20 20 "solid" "red")
-                            (rectangle 10 10 "solid" "red") ) true)
+;(define (image-larger img1 img2) true)                                  ;(stub)
 
-(check-expect (image-larger? (rectangle 20 10 "solid" "red")
-                            (rectangle 10 20 "solid" "red") ) false)
-
-;;Image Image -> Boolean                                        (signature)
-;;Returns true if the first image is larger that the second     (purpose)
-
-;(define (image-larger img1 img2) true)  ;(stub)
-
-;(define (image-larger img1 img2)        ;(template)
+;(define (image-larger img1 img2)                                        ;(template)
 ;  (... img1 img2))
 
-(define (image-larger? img1 img2)        ;(function body)
-  (> (* (image-height img1) (image-width img1) )
-     (* (image-height img2) (image-width img2) )))
+(define (image-larger? img1 img2)                                        ;(function body)
+  (and (> (image-height img1) (image-height img2) )
+       (> (image-width img1) (image-width img2) )))
